5efbb1e578a56162dac3c95e6b8d020e45bfecf0
Swathi <ssspriyamoru@gmail.com>
2023-11-10 12:02:16 +0530

    headerhome created

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Login from "./components/Login";
+import HeaderHome from "./components/HeaderHome";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Router>
         <Routes>
           <Route exact path="/" element={<Login/>}></Route>
+          <Route path="/home" element={<HeaderHome/>}></Route>
         </Routes>
       </Router>
     </div>
