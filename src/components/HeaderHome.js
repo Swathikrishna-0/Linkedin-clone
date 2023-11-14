cd9ac43ed60d9c14b9362d917c07940eae2fa1cc
Swathi <ssspriyamoru@gmail.com>
2023-11-14 11:32:32 +0530

    Navbar completed

--- src/components/HeaderHome.js
+++ src/components/HeaderHome.js
@@ -2,7 +2,7 @@ import React from "react";
 import Header from "./Header";
 import Home from "./Home";
 
-const HeaderHome = () => {
+const HeaderHome = (props) => {
   return (
     <div>
       <Header />
